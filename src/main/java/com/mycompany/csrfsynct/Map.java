8a0819ccbe0cb80a9f469af37026647949669238
Budditha Rathnayaka <36528636+BBRathnayaka@users.noreply.github.com>
2018-10-10 11:12:23 +0530

    Add files via upload

---
+++ src/main/java/com/mycompany/csrfsynct/Map.java
@@ -0,0 +1,16 @@
+
+package com.mycompany.csrfsynct;
+
+import java.util.HashMap;
+
+public class Map {
+	public static HashMap<String, String> hashMap = new HashMap<String, String>();
+	
+	public void setValue(String key,String value){
+		hashMap.put(key, value);
+	}
+	
+	public String getValue(String key){
+		return hashMap.get(key);
+	}
+}
